92093c9af443846ff33b7ae31a7004697e56d16e
santubangalore <santutsk@gmail.com>
2019-07-18 14:51:12 +0530

    first commit counter app

---
+++ src/components/counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = { 
+        count:2,
+        imageurl:"https://picsum.photos/200",
+       
+        tags : ["tag1","tag2", "tag3" ] 
+
+     };
+     styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    };
+
+    render() { 
+        return (
+        <div>
+            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul> 
+                { this.state.tags.map(tag=><li key={tag}> {tag} </li>) }
+            </ul>
+        </div>
+        );
+        
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count == 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount()
+    {
+        const {count}= this.state;
+        return count===0 ? "Zero": count;
+    }
+}
+ 
+export default Counter;
